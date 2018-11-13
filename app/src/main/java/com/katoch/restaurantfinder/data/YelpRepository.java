28da3e3f443f859f632df58f39b5ee66c075eb36
Munish Katoch <katochmunish14@gmail.com>
2018-11-13 09:27:39 -0500

    Adding RxJava dependency.

--- app/src/main/java/com/katoch/restaurantfinder/data/YelpRepository.java
+++ app/src/main/java/com/katoch/restaurantfinder/data/YelpRepository.java
@@ -7,9 +7,12 @@ import java.util.Map;
 
 import javax.inject.Inject;
 
+import io.reactivex.Observable;
+
 //ToDo No retrofit function.
 import retrofit2.Call;
 import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class YelpRepository {
@@ -19,7 +22,7 @@ public class YelpRepository {
     public Webservice mWebservice = (new WebModule()).getWebService();
 
 
-    public void getBusinessesSortByCategory(String latitude, String longitude, Callback<YelpSearchResponse> callback) {
+    public Observable<YelpSearchResponse> getBusinessesSortByCategory(String latitude, String longitude) {
 
         Map<String, String> params = new HashMap<>();
         params.put("term", "restaurants");
@@ -27,13 +30,28 @@ public class YelpRepository {
         params.put("longitude", longitude);
         params.put("limit", "50");
 
+        Callback<YelpSearchResponse> callback = new Callback <YelpSearchResponse>() {
+            @Override
+            public void onResponse(Call<YelpSearchResponse> call, Response<YelpSearchResponse> response) {
+                YelpSearchResponse response1 = response.body();
+                //mActivityView.setBusinessesInfo(response1.getBusinessesSortByCategory());
+            }
+            @Override
+            public void onFailure(Call<YelpSearchResponse> call, Throwable t) {
+                //mActivityView.onFailure(mContext.getString(com.katoch.restaurantfinder.R.string.error_failure_yelp_response) + t.getMessage());
+            }
+        };
+
         Call<YelpSearchResponse> call = mWebservice.getBusinessSearch(params);
         call.enqueue(callback);
+        //return Observable.fromCallable(callback);
+        return null;
     }
 
-    public void getBusinessPhotos(String businessId, Callback<BusinessDetail> callback) {
+    public Observable<BusinessDetail> getBusinessPhotos(String businessId, Callback<BusinessDetail> callback) {
         Call<BusinessDetail> call = mWebservice.getBusinessPhotos(businessId);
         call.enqueue(callback);
+        return null;
     }
 
     public void cancelOngoingCommand() {
